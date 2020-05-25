3b455f151a7574e5f2b027e58426518c43da4fc0
krote <krote0924@gmail.com>
2020-05-25 23:21:25 +0900

    react router is done

--- react_router/src/js/pages/Layout.js
+++ react_router/src/js/pages/Layout.js
@@ -1,10 +1,26 @@
 import React from "react";
+import { Link, withRouter } from "react-router-dom";
 
-export default class Layout extends React.Component {
+class Layout extends React.Component {
+  navigate(){
+    console.log(this.props.history);
+    this.props.history.push("/");
+  }
   render() {
     return (
-      <h1>KillerNews.net</h1>
+      <div>
+        <h1>KillerNews.net</h1>
+        {this.props.children}
+        <Link to="/archives/some-other-articles?date=yesterday&filter=none" class="btn btn-warning">archives (some other article)</Link>
+        <Link to="/archives?date=today&filter=hot"><button class="btn btn-danger">archives</button></Link>
+        <Link to="/settings/main"><button class="btn btn-success">settings</button></Link>
+        <Link to="/settings/extra"><button class="btn btn-success">settings(extra)</button></Link>
+        <button class="btn btn-error">OK</button>
+        <button class="btn btn-info" onClick={this.navigate.bind(this)}>featured</button>
+      </div>
     );
   }
 }
+
+export default withRouter(Layout);
 
